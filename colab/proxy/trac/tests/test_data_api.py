0dacc63d9252df92171fb2543582d04cbd2f505c
Thiago Ribeiro <thiagitosouza@hotmail.com>
2015-02-11 15:44:40 -0200

    Add test for dict_fetch_all using fixtures

---
+++ colab/proxy/trac/tests/test_data_api.py
@@ -0,0 +1,31 @@
+"""
+Test DataAPI class.
+Objective: Test parameters and behavior.
+"""
+from colab.proxy.trac.data_api import TracDataAPI
+
+from django.db import connections
+from django.test import TestCase
+
+
+class TracDataAPITest(TestCase):
+
+    fixtures = ["trac_data.json"]
+    def setUp(self):
+        self.connection = connections['default']
+        self.cursor = self.connection.cursor()
+
+    def tearDown(self):
+        self.connection.close()
+
+    def test_dict_fetch_all(self):
+        trac_dict= TracDataAPI()
+        self.cursor.execute(''' SELECT * FROM trac_wiki;''')
+        dict_result = trac_dict.dictfetchall(self.cursor)
+        self.assertIn('collaborators', dict_result[0])
+
+    def test_dict_fetch_all_is_dict(self):
+        trac_dict= TracDataAPI()
+        self.cursor.execute(''' SELECT * FROM trac_wiki;''')
+        dict_result = trac_dict.dictfetchall(self.cursor)
+        self.assertIsInstance(dict_result[0], dict)
